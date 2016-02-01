f24bdf1cf80eda8ec808121ce19ab36d2f8fb195
Aivaras Čenkus <aivaras.cenkus@gmail.com>
2016-02-01 20:48:41 +0200

    dev sync...

---
+++ src/jquery.upbro.js
@@ -0,0 +1,20 @@
+(function ( $ ) {
+    var upbro = function(options) {
+        // var feuturejs;
+ 
+        // This is the easiest way to have default options.
+        // var settings = $.extend({
+        //     // These are the defaults.
+
+        //     feuturejs:   [
+        //         'historyAPI', 
+        //         'placeholder'
+        //     ]
+        // }, options );
+ 
+        // Greenify the collection based on the settings variable.
+        // return settings.feuturejs;
+    console.log(options);
+    };
+ 
+}( jQuery ));
