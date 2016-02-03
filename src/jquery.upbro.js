4b02e793bfb88f576dc20e080014c777e5e9301e
Aivaras Čenkus <aivaras.cenkus@gmail.com>
2016-02-03 16:00:03 +0200

    dev sync: plugin prefixes...

--- src/jquery.upbro.js
+++ src/jquery.upbro.js
@@ -103,28 +103,28 @@ var upbro = function(options) {
   };
 
   var append_content = function () {
-    var html_template = '<div class="check_feature-container">';
-    html_template += '<a href="#" class="check_feature-close">';
-    html_template += '<img src="../src/images/cross.png" alt="Close">';
+    var html_template = '<div class="upbro_main-container">';
+    html_template += '<a href="#" class="upbro_close">';
+    html_template += '<img src="' + script_path + 'images/upbro_cross.png" alt="Close">';
     html_template += '</a>';
-    html_template += '<div class="description-container">';
-    html_template += '<div class="description">';
-    html_template += '<div class="container">';
-    html_template += '<div class="description-title">';
+    html_template += '<div class="upbro_description-container">';
+    html_template += '<div class="upbro_description">';
+    html_template += '<div class="upbro_container">';
+    html_template += '<div class="upbro_description-title">';
     html_template += lang('browser_title');
     html_template += '</div>';
-    html_template += '<div class="description-content">';
+    html_template += '<div class="upbro_description-content">';
     html_template += '<p>';
     html_template += '<strong>';
     html_template += lang('browser_old');
     html_template += '</strong>';
     html_template += lang('browser_dont_work');
     html_template += '</p>';
-    html_template += '<p class="please">';
+    html_template += '<p class="upbro_please">';
     html_template += lang('browser_please');
     html_template += '</p>';
     html_template += '</div>';
-    html_template += '<div class="browsers-container clearfix">';
+    html_template += '<div class="upbro_browsers-container clearfix">';
     html_template += '</div>';
     html_template += '</div>';
     html_template += '</div>';
@@ -136,11 +136,11 @@ var upbro = function(options) {
 
       $('body').prepend(html_template);
 
-      $('.check_feature-close').on('click', function(e) {
+      $('.upbro_close').on('click', function(e) {
         e.preventDefault();
 
         create_cookie('check_feature', 1, 1);
-        $('.check_feature-container').fadeOut();
+        $('.upbro_container').fadeOut();
       });
 
     }
@@ -150,22 +150,22 @@ var upbro = function(options) {
     var data_el;
     $.getJSON(script_path + 'browsers.json', function(data) {
       $.each(data, function(i, data) {
-        data_el = '.browsers-container';
+        data_el = '.upbro_container';
 
-        data_template = '<div class="browsers-item">';
-        data_template += '<div class="browser-image">';
-        data_template += '<img src="../src/images/' + data.image + '.png" alt="' + data.title + '">';
+        data_template = '<div class="upbro_browsers-item">';
+        data_template += '<div class="upbro_browser-image">';
+        data_template += '<img src="' + script_path + 'images/' + data.image + '.png" alt="' + data.title + '">';
         data_template += '</div>';
-        data_template += '<div class="browser-title">';
+        data_template += '<div class="upbro_browser-title">';
         data_template += data.title;
         data_template += '</div>';
-        data_template += '<div class="browser-bottom">';
-        data_template += '<a target="_blank" href="' + data.download_link + '" class="browser-download">';
+        data_template += '<div class="upbro_browser-bottom">';
+        data_template += '<a target="_blank" href="' + data.download_link + '" class="upbro_browser-download">';
         data_template += lang('browser_download');
         data_template += '</a>';
         data_template += '</div>';
         data_template += '</div>';
-        if ($('.check_feature-container').length) {
+        if ($(data_el).length) {
           $(data_template).appendTo(data_el);
         }
       });
